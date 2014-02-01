77f83a3184bd70f69e790d92dddfae924a85a298
Juan Pablo Blanco <j.pabloblanco@gmail.com>
2014-02-01 17:40:26 -0200

    luchando con la cinta transportadora

---
+++ Assets/BeltController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeltController : MonoBehaviour {
+
+	void Update () {
+	
+	}
+ 
+  void OnCollisionStay(Collision collisionInfo) {
+    //collisionInfo.rigidbody.velocity = 10 * transform.right;
+    collisionInfo.rigidbody.AddForce(Vector3.right*10);
+  }
+
+
+}
