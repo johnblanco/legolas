30bc9a9ac56dba0cb40fcf8a286803de55762bc6
Juan Pablo Blanco <j.pabloblanco@gmail.com>
2014-02-02 23:44:07 -0200

    hice andar la cinta!

---
+++ Assets/Scripts/BeltLimitCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeltLimitCollision : MonoBehaviour {
+  public Transform stepSpawn;
+  public GameObject step;
+
+  void OnTriggerEnter(Collider other) {
+    if(other.tag == "Step"){
+      Destroy(other.gameObject);
+      //tengo que spawnear un nuevo step en StepSpawn
+      Instantiate(step, stepSpawn.position, Quaternion.identity);
+    }
+      
+
+    
+  }
+
+
+}
---
+++ Assets/Scripts/BeltStep.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeltStep : MonoBehaviour {
+	public float speed;
+
+  void Update(){
+    transform.Translate(Time.deltaTime * speed, 0,0 );
+  }
+
+}
